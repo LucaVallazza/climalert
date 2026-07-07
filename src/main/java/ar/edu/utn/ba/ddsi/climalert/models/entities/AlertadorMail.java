e053ea00a07e6e8f2432030509ab7933279895d2
LucaVallazza <Luvallazza@gmail.com>
2026-07-07 02:05:45 -0300

    alertadorMail base

---
+++ src/main/java/ar/edu/utn/ba/ddsi/climalert/models/entities/AlertadorMail.java
@@ -0,0 +1,21 @@
+package ar.edu.utn.ba.ddsi.climalert.models.entities;
+import ar.edu.utn.ba.ddsi.climalert.services.MailService;
+
+public class AlertadorMail implements Alertador{
+    private String mail;
+    private MailService mailService;
+
+    public AlertadorMail(String mail, MailService mailService){
+        this.mail = mail;
+        this.mailService = mailService;
+    }
+
+    public AlertadorMail(String mail) {
+        this.mail = mail;
+    }
+
+    @Override
+    public void enviarAlerta() {
+        mailService.enviarMail(mail);
+    }
+}
--- src/main/java/ar/edu/utn/ba/ddsi/climalert/services/MailService.java
+++ src/main/java/ar/edu/utn/ba/ddsi/climalert/services/MailService.java
@@ -1,5 +1,12 @@
 package ar.edu.utn.ba.ddsi.climalert.services;
 
+import org.springframework.stereotype.Service;
+
 // Se comunica con los mails
+@Service
 public class MailService {
+
+    public void enviarMail(String direccion){
+        System.out.println("Enviar mail a :" + direccion);
+    }
 }
