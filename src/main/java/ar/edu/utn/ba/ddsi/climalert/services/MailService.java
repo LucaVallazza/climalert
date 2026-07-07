b28579b59f01b6a98efcc17865a0c759568c704f
LucaVallazza <Luvallazza@gmail.com>
2026-07-07 00:18:00 -0300

    initial commit

---
+++ src/main/java/ar/edu/utn/ba/ddsi/climalert/services/MailService.java
@@ -0,0 +1,5 @@
+package ar.edu.utn.ba.ddsi.climalert.services;
+
+// Se comunica con los mails
+public class MailService {
+}
---
+++ src/main/java/ar/edu/utn/ba/ddsi/climalert/services/WeatherService.java
@@ -0,0 +1,5 @@
+package ar.edu.utn.ba.ddsi.climalert.services;
+
+// Traer la información
+public class WeatherService {ss
+}
